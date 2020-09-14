406d8de90102cd88106c561db9895978cd25b263
daniel molina <bigdpower@icloud.com>
2020-09-14 15:07:15 -0700

    added navbar and footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import  Navbar from 'react-bootstrap/Navbar';
+
+function footer() {
+    return (
+        <>
+          <Navbar bg="dark" fixed="bottom" variant="dark" >
+    <Navbar.Brand>PIAG MUSEUM copyright 2020</Navbar.Brand>
+  </Navbar>  
+  </>
+    )
+}
+
+export default footer
